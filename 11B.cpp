e6e9bbf3b5f7e964e17cbff57f72458bad535680
Swayam Patel <123808643+Swayam-patel19@users.noreply.github.com>
2024-09-06 12:06:18 +0530

    Add files via upload

---
+++ 11B.cpp
@@ -0,0 +1,23 @@
+// NAME - Swayam Patel
+// PRN - 23070123165
+// EXPERIMENT - 11(B) 
+
+# include<iostream>
+using namespace std;
+class cuboid
+{
+    public:
+    double h = 2.0,b = 3.0,l = 4.0;
+    double volume()
+    {
+        double vol = h * b * l;
+        cout<<"Volume "<<vol<<endl;
+    }
+
+};
+int main()
+{
+    cuboid c1;
+    c1.volume();
+  
+}
